ff6e8cc32cdef1ed22ceffb0dc89e30dce302185
logzinga <65839350+logzinga@users.noreply.github.com>
2023-02-21 13:00:08 +1100

    main menu updates, should work now

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Host {
-
+    public void HostGame () {
         SceneManager.LoadScene("Level01");
-
+    }
+    public void JoinGame () {
+        SceneManager.LoadScene("Level01");
+    }
+    public void ServerMode () {
+        SceneManager.LoadScene("Level01");
+    }
+    public void QuitGame () {
+        Debug.Log("Exiting Game...");
+        Application.Quit();
     }
 }
